b8448cb449138bc2c427dc16f779e3e98587cd1a
Oscar GG <profesor.oscar.gomez@gmail.com>
2020-02-06 21:15:02 +0100

    Bad expanded blocks

--- pyliton.py
+++ pyliton.py
@@ -10,10 +10,11 @@ Usage: pyliton <filename>
 """
 
 
-REGEX_DEFINE_BLOCK="^---(?P<blockname>[a-zA-Z0-9 ]+)---(?P<language>[a-zA-Z]*)"
+REGEX_BLOCK_NAME="(?P<blockname>[a-zA-Z0-9 ]+)"
+REGEX_DEFINE_BLOCK="^---"+REGEX_BLOCK_NAME+"---(?P<language>[a-zA-Z]*)"
 REGEX_DEFINE_END_BLOCK="^---$"
 
-
+REGEX_MACRO_TO_EXPAND="@\{"+REGEX_BLOCK_NAME+"\}"
 STATE_READING_LINES=0
 STATE_READING_BLOCK=STATE_READING_LINES+1
 
@@ -35,10 +36,11 @@ class Block(object):
         self.lines=[]
     def append_line(self, line):
         self.lines.append(line)
+    def get_name(self):
+        return self.name
     def __str__(self):
-        text="--Block:{0}\n".format(self.name)
+        text=""
         text+="".join(self.lines)
-        text+="--End block"
         return text
 
 def read_defined_blocks(lines):
@@ -74,15 +76,43 @@ def read_defined_blocks(lines):
     #End of while
     return blocks
 
+
+def get_text_from_list_of_blocks(block_name, defined_blocks):
+    for b in defined_blocks:
+        if b.get_name()==block_name:
+            return b.__str__()
+    return "Undefined block:"+block_name
+
+def expand_blocks(lines, defined_blocks):
+    result_text=""
+    re_expand_block         =   re.compile(REGEX_DEFINE_BLOCK)
+    counter=0
+    max_lines=len(lines)
+    
+    while counter < max_lines:
+        current_line=lines[counter]
+        result=re_expand_block.match(current_line)
+        if result!=None:
+            block_name=result.group("blockname")
+            print("Expandiendo:"+block_name)
+            expanded_text=get_text_from_list_of_blocks(block_name, defined_blocks)
+            result_text=result_text+expanded_text
+            counter=counter+1
+        else:
+            result_text=result_text+current_line
+        counter=counter+1
+    #End of while
+    return result_text
+
 def pyliton(filename):
     #print(filename)
     with open(filename) as file:
         lines=file.readlines()
         defined_blocks=read_defined_blocks(lines)
-        print("Blocks")
-        for b in defined_blocks:
-            print(b)
         
+        text=expand_blocks(lines, defined_blocks)
+        print("Expanded")
+        print (text)
 
 if __name__ == "__main__":
     if len(sys.argv)!=2:
